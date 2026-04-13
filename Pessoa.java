7fec0c186fa5daf7742c7b865edeee7e9078093a
yasmininsouza <yasmin.7875708@aluno.mg.gov.br>
2026-04-13 08:21:51 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+package Testes;
+public class Pessoa {
+String nome;
+int idade;
+        public void caminhar() {
+        System.out.println("caminhando...");
+        }
+        public void dormir() {
+        System.out.println("dormir...");
+        }
+}
